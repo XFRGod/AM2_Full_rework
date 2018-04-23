23dfd783925912d659f0105b20a7109d8b877bce
Menyhárt Róbert <XFRGod72@gmail.com>
2018-04-23 16:12:17 +0200

    Rudimentary mana regen

--- src/main/java/am2/handler/EventHandler.java
+++ src/main/java/am2/handler/EventHandler.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class EventHandler {
     @SubscribeEvent
@@ -14,5 +15,11 @@ public class EventHandler {
         IMana mana = player.getCapability(ManaProvider.MANA_CAP, null);
         String message = String.format("Hello, you have §7%d§r mana left.", (long) mana.getCurrentMana());
         player.sendMessage(new TextComponentString(message));
+    }
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event){
+        EntityPlayer player = event.player;
+        IMana mana = player.getCapability(ManaProvider.MANA_CAP, null);
+        if (mana.getMaxMana() > mana.getCurrentMana()) mana.setCurrentMana(mana.getCurrentMana()+1);
     }
 }

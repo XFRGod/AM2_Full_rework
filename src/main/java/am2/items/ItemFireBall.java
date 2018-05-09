476eb75c1e571f3a00bf0a48884cca720cb89286
XFRGod <xfrgod72@gmail.com>
2018-05-09 22:20:51 +0200

    Fixed long standing typo

--- src/main/java/am2/items/ItemFireBall.java
+++ src/main/java/am2/items/ItemFireBall.java
@@ -14,7 +14,7 @@ import net.minecraft.world.World;
 public class ItemFireBall extends AM2Item {
 
     public ItemFireBall() {
-        super("firewall");
+        super("fireball");
     }
 
     @Override
